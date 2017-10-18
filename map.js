48226ae38417a4078483d7d866364b76d3257391
Jonatan Granados <joni2granados@gmail.com>
2017-10-18 09:33:55 +0200

    Modificación para examen. Quitados controles de capas y capas. Zoom permitido. Añadidos circulos de zona. Modificados los títulos del documento y la web. Corregida la separación de archivos

--- map.js
+++ map.js
@@ -1,37 +1,43 @@
 window.onload = function () {
-	var normal   = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-					attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-					maxZoom: 15
-				});
-		topo  = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-				attribution: 'Map data &copy; <a href="http://openstreemap.org">OpenStreetMap</a>',
-				maxZoom: 15
+	var mapa   = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				});
 
-	var map = L.map('mapid', {
-		zoomControl:false, 
+	var map = L.map('mapid', { 
 		dragging:false,
-		layers: [normal, topo]
+		layers: [mapa]
 		});
 
-	map.scrollWheelZoom.disable();
 	map.locate({setView:true})
 		.on('locationfound', function(e){
 		            var marker = L.marker([e.latitude, e.longitude]).bindPopup('Estas aquí');
-		            var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+		            var precision = L.circle([e.latitude, e.longitude], e.accuracy/2, {
 		                weight: 1,
 		                color: 'blue',
 		                fillColor: '#dadada',
 		                fillOpacity: 0.6
 		            });
+		            var cirExterior = L.circle([e.latitude, e.longitude], e.accuracy*2000, {
+		            	stroke: false,
+		            	fill: true,
+		            	color: '#fc6703',
+		            	fillOpacity: 0.2
+		            });
+		            var cirMedio = L.circle([e.latitude, e.longitude], e.accuracy*500, {
+		            	stroke: false,
+		            	fill: true,
+		            	color: '#fc6703',
+		            	fillOpacity: 0.4
+		            });
+		            var cirInterior = L.circle([e.latitude, e.longitude], e.accuracy*125, {
+		            	stroke: false,
+		            	fill: true,
+		            	color: '#fc6703',
+		            	fillOpacity: 0.6
+		            });
 		            map.addLayer(marker);
-		            map.addLayer(circle);
+		            map.addLayer(precision);
+		            map.addLayer(cirExterior);
+		            map.addLayer(cirMedio);
+		            map.addLayer(cirInterior);
 		});
-
-	var baseLayers = {
-			"Normal": normal,
-			"Topo": topo
-		};
-
-	L.control.layers(baseLayers).addTo(map);
 }
